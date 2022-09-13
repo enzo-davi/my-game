69688dde0b591dae7fe67ab7bf225a6c27df2023
enzoocker <enzo.o@aluno.ifsc.edu.br>
2022-09-13 19:14:18 +0000

    atualizado

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,13 @@ from dotenv import load_dotenv
 import discord
 load_dotenv()
 
+import random
+lista_juca = ['melancia com mel', 'the rock cocaina' , 'pequenoi grande anão' , 'pimenta com granola' , 'falo tudo que penso']
+lista_enzo = ['branquinho básico']
+lista_davi = ['criança grande']
+lista_nico = ['nariz']
+lista_pedro = ['alcólatra']
+lista_vi = ['bebado triste']
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -14,9 +21,23 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        if message.content.startswith('!enzo'):
-            await message.reply('Hello!', mention_author=True)
+        if message.content.startswith('.juca'):
+            await message.reply( random.choice(lista_juca), mention_author=True)
 
+        if message.content.startswith('.enzo'):
+            await message.reply( random.choice(lista_enzo), mention_author=True)
+
+        if message.content.startswith('.davi'):
+            await message.reply( random.choice(lista_davi), mention_author=True)
+
+        if message.content.startswith('.nico'):
+            await message.reply( random.choice(lista_nico), mention_author=True)
+
+        if message.content.startswith('.pedro'):
+            await message.reply( random.choice(lista_pedro), mention_author=True)
+
+        if message.content.startswith('.vi'):
+            await message.reply( random.choice(lista_vi), mention_author=True)
 
 intents = discord.Intents.default()
 intents.message_content = True
