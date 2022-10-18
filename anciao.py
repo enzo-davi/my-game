bb7fec56f20253ebb78d658dce1b40d17f701fe8
Davinator <davi.sj@aluno.ifsc.edu.br>
2022-10-18 20:17:41 +0000

    reação e maquina de estado

--- anciao.py
+++ anciao.py
@@ -4,37 +4,104 @@ import discord
 from discord import FFmpegPCMAudio
 import random
 import asyncio
+from os.path import exists
+from re import fullmatch
+from estados_davi import estados, partidas
 from discord.ext import commands
 load_dotenv()
 
 imagens = ['batman.jpg','lagartixa.jpg','mordecai.jpg','trump.jpeg']
+
+
+
 
 class MyBot(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-    async def on_message(self, message):
+    async def on_message(self, msg):
         # we do not want the bot to reply to itself
-        channel = message.channel
+        channel = msg.channel
+        autor = msg.author.id
 
-        if message.author.id == self.user.id:
+        if msg.author.bot:
             return
-        sent = await message.reply('oiii!!!!111')
-        await sent.add_reaction("✅")
-        await sent.add_reaction("❌")
+        
+        if autor not in partidas:
+        #
+        # Jogador começa no estado 0 com duas chaves
+            partidas[autor] = {
+            'estado': 0,
+            'inventario': {
+                'chave_prateada',
+                'chave_dourada'
+                }
+            }
+        
 
+        estado_do_jogador = estados[partidas[autor]['estado']]
+        inventario_do_jogador = partidas[autor]['inventario']
+        
+        for key, value in estado_do_jogador['proximos_estados'].items():
+            if fullmatch(key, msg.content):
+                #
+                # Verificar se o jogador possui inventário mínimo para avançar
+                if inventario_do_jogador.issuperset(estados[value]['inventario']):
+                    #
+                    # Atualiza o estado do jogador
+                    partidas[autor]['estado'] = value
+                    #
+                    # Remove os itens de inventário requisitados
+                    #partidas[autor]['inventario'] = inventario_do_jogador.difference(
+                        #estados[value]['inventario'])
+                    #
+                    # Se houver uma imagem referente ao estado,
+                    # envia essa primeiro
+                    imagem = str(value) + '.png'
+                    if exists(imagem):
+                        await msg.channel.send(file=discord.File(imagem))
+                    #
+                    # Se houver um som referente ao estado,
+                    # toca no canal de voz do jogador
+                    som = str(value) + '.opus'
+                    if exists(som):
+                        # canal_de_voz.play(AudioSource)
+                        pass
+                    
+                    # Cria uma lista de frases usando o delimitador '|' e envia uma a uma
+                    
+
+                    sent = await msg.channel.send (estados[value]['frases'])
+                    
+                    await sent.add_reaction("✅")
+                    await sent.add_reaction("❌")
+
+
+                else:
+                    #
+                    # Retornar mensagem (e manter jogador no atual estado)
+                    await msg.channel.send(frases['inventario_insuficiente'])
+                return
+
+
+
+        
+        
 
 
         async def check(reaction, user):
-            return user != message.author and str(reaction.emoji) == '✅'
+            return user != msg.author and str(reaction.emoji) == '✅'
         
-        try:
-            reaction, user = await client.wait_for('reaction_add', timeout=120.0, check=check)
-        except asyncio.TimeoutError:
-            await channel.send('👎')
-        else:
-            await channel.send('👍')    
+            reaction, user = await client.wait_for('reaction_add', check=check)
+        #except asyncio.TimeoutError:
+        #    await channel.send('👎')
+        #else:
+        #    await channel.send('👍')
+
+
+        if msg.author.id not in partidas:
+            partidas[msg.author.id] = 0           
 
 
 
---
+++ estados_davi.py
@@ -0,0 +1,29 @@
+estados = {
+    0:{
+        "frases":"oioi hahah", 
+        "proximos_estados":{
+            "✅": 1,
+            "❌":2,
+            "inventario": {}
+            
+        }
+    },
+    1:{
+        "frases":"bom dia",
+        "proximos_estados":{
+            "✅":1
+        },
+        "inventario": {}
+
+
+    },
+    2:{
+        "frases":"boa noite",
+        "proximos_estados":{"✅":1
+
+    },
+    "inventario": {}
+}
+}
+partidas = {}
+
